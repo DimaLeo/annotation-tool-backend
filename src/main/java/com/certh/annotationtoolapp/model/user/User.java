58933d3b4833fb8f732760d9201f74587ed340c3
dimaleo95 <dim.leon@iti.gr>
2023-11-01 13:35:32 +0200

    fix

--- src/main/java/com/certh/annotationtoolapp/model/user/User.java
+++ src/main/java/com/certh/annotationtoolapp/model/user/User.java
@@ -6,9 +6,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.beans.ConstructorProperties;
 import java.util.Collection;
