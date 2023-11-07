54fd2c256323cd809370a2ebbe73c2d121c97508
dimaleo95 <dim.leon@iti.gr>
2023-11-07 17:22:04 +0200

    merged changes of fetching usecases from db

--- src/main/java/com/certh/annotationtoolapp/controller/UsecaseController.java
+++ src/main/java/com/certh/annotationtoolapp/controller/UsecaseController.java
@@ -5,14 +5,12 @@ import com.certh.annotationtoolapp.service.UsecaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/usecases")
 public class UsecaseController {
     private final UsecaseService usecaseService;
--- src/main/java/com/certh/annotationtoolapp/model/usecase/Usecase.java
+++ src/main/java/com/certh/annotationtoolapp/model/usecase/Usecase.java
@@ -1,9 +1,11 @@
 package com.certh.annotationtoolapp.model.usecase;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
+@Data
 @Document(collection = "UsecaseOptions")
 public class Usecase {
 
@@ -12,46 +14,6 @@ public class Usecase {
     private String value;  
     private String label;
     private List<LanguageOption> language;
-
-    public Usecase() {}
-
-    public Usecase(String value, String label, List<LanguageOption> language) {
-        this.value = value;
-        this.label = label;
-        this.language = language;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public List<LanguageOption> getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(List<LanguageOption> language) {
-        this.language = language;
-    }
 
     @Override
     public String toString() {
